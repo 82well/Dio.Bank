4f863e33c57b2424535a453937f2e11d0cf3f4c4
82well <72098782+82well@users.noreply.github.com>
2022-07-06 22:02:50 -0300

    Add files via upload

---
+++ Classes/conta/conta.cs
@@ -0,0 +1,21 @@
+using System.Reflection.Emit;
+using System.Data;
+using System;
+
+     
+namespace DIO.conta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta minhaConta = new Conta();
+            minhaConta.Nome = "Wellington";
+
+            
+            Console.WriteLine("Ola");
+
+
+        }
+    }
+}
